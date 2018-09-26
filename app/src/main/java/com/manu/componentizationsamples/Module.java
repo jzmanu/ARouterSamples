5d1c4ac0cd695f8a7b1cb7300d1f3aedd8fd6df7
jzman <18393817540@163.com>
2018-09-26 17:15:59 +0800

    first commit

---
+++ app/src/main/java/com/manu/componentizationsamples/Module.java
@@ -0,0 +1,10 @@
+package com.manu.componentizationsamples;
+
+/**
+ * Powered by jzman.
+ * Created on 2018/9/26 0026.
+ */
+public interface Module {
+    String MODULE_ONE = "/module1/module-one";
+    String MODULE_TWO = "/module2/module-two";
+}
